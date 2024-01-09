14529d5841e8c82dbd4751d9944d69d448b2aee4
fernandoEncalada <ferencalada1@gmail.com>
2024-01-08 23:31:24 -0500

    add config server and routes

--- course-microservice/src/main/java/com/microservice/course/coursemicroservice/client/StudentClient.java
+++ course-microservice/src/main/java/com/microservice/course/coursemicroservice/client/StudentClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
-@FeignClient(name = "msvc-student", url = "localhost:8090/api/student")
+@FeignClient(name = "msvc-student", url = "localhost:8081/api/student")
 public interface StudentClient {
 
     @GetMapping("/search-by-course/{idCourse}")
